3bb0a4dd7163e529d7e90bcd4948e03894cc31f9
baminou <brice.aminou@gmail.com>
2018-09-17 11:48:10 -0400

    Readme text from .rst for pypi

--- setup.py
+++ setup.py
@@ -6,10 +6,9 @@ setup(
     version='0.0.2',
     packages=find_packages(),
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description="test",
+    long_description=open('README.rst').read(),
     author='Brice Aminou',
     author_email='brice.aminou@gmail.com',
     keywords='workflow tool',
-    url='https://github.com/baminou/Uperations-Base',
-    package_dir={'mmm':'.'}
+    url='https://github.com/baminou/Uperations-Base'
 )
