779386e9ea4074bad0a9cf9f15d87088a16b09de
baminou <brice.aminou@gmail.com>
2018-09-20 11:18:15 -0400

    version updated

--- libraries/uperation_base/setup.py
+++ libraries/uperation_base/setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name='uperations-base',
-    version='0.0.4',
+    version='0.0.5',
     packages=find_packages(),
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open(os.path.join(os.path.dirname(os.path.realpath(__file__)),'README.rst')).read(),
