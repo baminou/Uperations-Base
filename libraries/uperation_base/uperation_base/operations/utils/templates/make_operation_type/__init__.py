5247f8c8a764a891163aece6fcd050f636029f72
baminou <brice.aminou@gmail.com>
2018-10-30 14:39:06 -0400

    Fix of operation type template to have more informations

--- libraries/uperation_base/uperation_base/operations/utils/templates/make_operation_type/__init__.py
+++ libraries/uperation_base/uperation_base/operations/utils/templates/make_operation_type/__init__.py
@@ -3,10 +3,13 @@ from uperations.operation import Operation
 class OPERATIONTYPE(Operation):
 
     def _parser(self, main_parser):
+        super(OPERATIONTYPE, self)._parser(main_parser)
+        #main_parser.add_argument('first_argument', help="Argparse argument example")
         return main_parser
 
     def _before_start(self):
         super(OPERATIONTYPE, self)._before_start()
+        #print(self.args.first_argument)
         return True
 
     def _on_running(self):
